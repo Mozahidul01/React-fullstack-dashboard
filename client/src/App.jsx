7a5d9105f3734787d2030376b361101ca5f7f53a
mozahidul01 <mozo.postbox@gmail.com>
2023-07-04 09:45:37 +0600

    Loading warning added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,29 +2,49 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import { useSelector } from "react-redux";
 import { themeSettings } from "./theme";
-import { useMemo } from "react";
+import { useEffect, useMemo } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import Dashboard from "./scenes/dashboard/";
-import Layout from "./scenes/layout";
-import Products from "./scenes/products";
-import Customers from "./scenes/customers";
-import Transactions from "./scenes/transactions";
-import Geography from "./scenes/geography";
-import Overview from "./scenes/overview";
-import Daily from "./scenes/daily";
-import Monthly from "./scenes/monthly";
-import Breakdown from "./scenes/breakdown";
-import Admin from "./scenes/admin";
-import Performance from "./scenes/performance";
+import Dashboard from "./pages/dashboard/";
+import Layout from "./pages/layout";
+import Products from "./pages/products";
+import Customers from "./pages/customers";
+import Transactions from "./pages/transactions";
+import Geography from "./pages/geography";
+import Overview from "./pages/overview";
+import Daily from "./pages/daily";
+import Monthly from "./pages/monthly";
+import Breakdown from "./pages/breakdown";
+import Admin from "./pages/admin";
+import Performance from "./pages/performance";
+import { Toaster, toast } from "react-hot-toast";
+import { Warning } from "@mui/icons-material";
 
 function App() {
   const { mode } = useSelector((state) => state.theme);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 
+  useEffect(() => {
+    toast(
+      "Please bear with us as the dashboard's backend is hosted on Render's free tier, which may cause slight delays in data loading.",
+      {
+        duration: 5000,
+        position: "top-center",
+        style: {
+          border: "1px solid #E7CB02",
+          padding: "14px",
+          color: "#311702",
+          minWidth: "375px",
+        },
+        icon: <Warning sx={{ color: "#E7CB02", fontSize: "1.5rem" }} />,
+      }
+    );
+  }, []);
+
   return (
     <BrowserRouter>
       <ThemeProvider theme={theme}>
         <CssBaseline />
+        <Toaster />
 
         <Routes>
           <Route element={<Layout />}>
